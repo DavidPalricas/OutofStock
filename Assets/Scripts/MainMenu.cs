24f84efe624f7fb2a0b9c660f9c89826fcf2a62e
simoH3945 <hstavares2002@gmail.com>
2025-05-03 18:08:41 +0100

    Main Menu Starters

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// The MainMenu class is responsible for having the buttons logic in the main menu.
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    //// Referencia ao objecto com o script que gere o fade in/out
+    //[SerializeField]
+    //private GameObject fadeManager;
+
+    //// Variavel que vai verificar qual a cena atual, DEVE EXISTIR NOS SCRIPTS DE CANVAS DAS CENAS QUE NECESSITAM DE DAR FADE IN/OUT
+    //private int currentSceneIndex;
+
+    /// <summary>
+    /// The PlayGame method is responsible for loading the first level of the game.
+    /// </summary>
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Utils.PlaySoundEffect("fall");
+        //fadeManager.GetComponent<LevelChanger>().FadeToLevel(currentSceneIndex + 1);
+    }
+
+    ///// <summary>
+    ///// The ControlsMenu method is responsible for loading the controls menu.
+    ///// </summary>
+    //public void ControlsMenu()
+    //{
+    //    SceneManager.LoadScene("ControlsMenu");
+    //}
+
+    /// <summary>
+    /// The QuitGame method is responsible for quitting the game.
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
